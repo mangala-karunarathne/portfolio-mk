58b795de3e5d905123a7a5595d766a44c6c44a6a
mangala-karunarathne <mangalaicc@gmail.comm>
2024-09-16 16:09:07 +0530

    CHG | Modify Tech Stack UI

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -111,32 +111,7 @@ export const BentoGridItem = ({
 
           {id == 2 && <GlobeDemo />}
           {id == 3 && (
-            // <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-            //   <div className="flex flex-col gap-1 lg:gap-8">
-            //     {["React.js", "Next.js", "TypeScript"].map((item) => (
-            //       <span
-            //         key={item}
-            //         className="py-2 lg:py-4 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-            //       >
-            //         {item}
-            //       </span>
-            //     ))}
-            //     <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
-            //   </div>
-            //   <div className="flex flex-col gap-3 lg:gap-3">
-            //     <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
-            //     {["Node.js", "AWS", "MongoDB"].map((item) => (
-            //       <span
-            //         key={item}
-            //         className="py-2 lg:py-3 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-            //       >
-            //         {item}
-            //       </span>
-            //     ))}
-            //   </div>
-            // </div>
-            <div className="flex gap-1 w-fit absolute -right-3 lg:-right-2">
-              {/* <div className="flex flex-col gap-1"> */}
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
               <div className="flex flex-col gap-1 lg:gap-3 lg:mt-3 lg:pb-4">
                 {["React.js", "Next.js", "TypeScript"].map((item) => (
                   <span
@@ -146,11 +121,10 @@ export const BentoGridItem = ({
                     {item}
                   </span>
                 ))}
-                {/* <span className="py-4 lg:py-5 px-3 rounded-lg text-center bg-[#10132e]" /> */}
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e] lg:mt-2 space-after mr-2" />
+                <span className="lg:py-7 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]" />
               </div>
               <div className="flex flex-col gap-1 lg:gap-3 lg:mt-3 lg:pb-4">
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e] lg:mt-2 space-after mr-2" />
+                <span className="lg:py-7 py-4 first-letter:py-4 px-3 rounded-lg text-center bg-[#10132e] lg:mb-2 space-after mr-2" />
                 {["Node.js", "AWS", "MongoDB"].map((item) => (
                   <span
                     key={item}
